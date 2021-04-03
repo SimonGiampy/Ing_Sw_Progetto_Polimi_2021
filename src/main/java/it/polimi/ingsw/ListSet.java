2cd8a5f210b64f4856fcee7fbf58aee2d3b107f5
SimonGiampy <simongiampydev@gmail.com>
2021-04-03 23:08:24 +0200

    added class to work on

--- src/main/java/it/polimi/ingsw/ListSet.java
+++ src/main/java/it/polimi/ingsw/ListSet.java
@@ -8,12 +8,12 @@ import java.util.ArrayList;
 public class ListSet {
 	
 	//TODO: check if the subList is strictly contained in the set
-	public static <T> boolean subset(ArrayList<T> subList, ArrayList<T> set) {
+	public static <T> void subset(ArrayList<T> subList, ArrayList<T> set) {
 		// hint: use hashMaps to create a mapping between the element objects
 	}
 	
 	//TODO: create function to calculate the number of times a certain element appears in a single list
-	public static <T> int count(ArrayList<T> set, T element) {
+	public static <T> void count(ArrayList<T> set, T element) {
 	
 	}
 }
