2517e00db5275c32166e59a4748e834a04774548
AlessandroGavardi <alessandro.gavardi@mail.polimi.it>
2021-04-14 15:29:11 +0200

    created package util created message enum and started  messageType

---
+++ src/main/java/message/LoginRequest.java
@@ -0,0 +1,15 @@
+package message;
+
+public class LoginRequest extends Message {
+
+	public LoginRequest(String nickname){
+		super(nickname,MessageType.LOGIN_REQUEST);
+	}
+
+	@Override
+	public String toString(){
+		return "LoginRequest{ "+
+				"nickname: "+ getNickname()+
+				'}';
+	}
+}
