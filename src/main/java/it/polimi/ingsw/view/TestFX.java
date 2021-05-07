e0f4d35e10805b580e00a3cfa43ebbdca1fd75d9
SimonGiampy <simongiampydev@gmail.com>
2021-05-07 13:32:25 +0200

    testing javafx

---
+++ src/main/java/it/polimi/ingsw/view/TestFX.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TestFX extends Application {
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Hello, World!");
+		primaryStage.show();
+	}
+}
