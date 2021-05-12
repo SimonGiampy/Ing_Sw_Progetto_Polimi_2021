c7a3edbb798b53795799a2fc83fca2352db1b9f5
AlessandroGavardi <alessandro.gavardi@mail.polimi.it>
2021-05-12 15:41:32 +0200

    added GUI elements

--- src/main/java/it/polimi/ingsw/view/gui/NumberOfPlayers.java
+++ src/main/java/it/polimi/ingsw/view/gui/NumberOfPlayers.java
@@ -1,17 +1,78 @@
 package it.polimi.ingsw.view.gui;
 
 import it.polimi.ingsw.observers.ViewObservable;
-import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import javafx.scene.image.ImageView;
 
-public class NumberOfPlayers extends ViewObservable implements Initializable, SceneController {
+public class NumberOfPlayers extends ViewObservable implements SceneController {
 
+	@FXML public ImageView onePlayerGlow;
+	@FXML public ImageView onePlayerNormal;
+	@FXML public ImageView twoPlayersNormal;
+	@FXML public ImageView twoPlayersGlow;
+	@FXML public ImageView threePlayersNormal;
+	@FXML public ImageView threePlayersGlow;
+	@FXML public ImageView fourPlayersNormal;
+	@FXML public ImageView fourPlayersGlow;
 
-	@Override
-	public void initialize(URL url, ResourceBundle resourceBundle) {
+	public void onMouseClickedOne(){
+		new Thread(() -> notifyObserver(obs -> obs.onUpdatePlayersNumber(1))).start();
+	}
 
+	public void onMouseClickedTwo(){
+		new Thread(() -> notifyObserver(obs -> obs.onUpdatePlayersNumber(2))).start();
+	}
+
+	public void onMouseClickedThree(){
+		new Thread(() -> notifyObserver(obs -> obs.onUpdatePlayersNumber(3))).start();
+	}
+
+	public void onMouseClickedOFour(){
+		new Thread(() -> notifyObserver(obs -> obs.onUpdatePlayersNumber(4))).start();
+	}
+
+	public void onMouseEnteredOne(){
+		//onePlayerNormal.setVisible(false);
+		onePlayerGlow.setVisible(true);
 
 	}
+
+	public void onMouseExitedOne(){
+		onePlayerGlow.setVisible(false);
+		//onePlayerNormal.setVisible(true);
+	}
+
+	public void onMouseEnteredTwo(){
+		twoPlayersGlow.setVisible(true);
+	}
+
+	public void onMouseExitedTwo(){
+		twoPlayersGlow.setVisible(false);
+		//onePlayerNormal.setVisible(true);
+	}
+
+	public void onMouseEnteredThree(){
+		threePlayersGlow.setVisible(true);
+	}
+
+	public void onMouseExitedThree(){
+		threePlayersGlow.setVisible(false);
+		//onePlayerNormal.setVisible(true);
+	}
+
+	public void onMouseEnteredFour(){
+		fourPlayersGlow.setVisible(true);
+	}
+
+	public void onMouseExitedFour(){
+		fourPlayersGlow.setVisible(false);
+		//onePlayerNormal.setVisible(true);
+	}
+
+	@FXML public void initialize(){
+
+	}
+
+
 }
